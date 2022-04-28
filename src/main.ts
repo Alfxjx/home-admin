c4570af68b97cab44f846b5b844b4bfa45720c8c
xujx <xjxtju@163.com>
2022-04-28 21:27:43 +0800

    chore: del prefix

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.setGlobalPrefix('home-api');
   await app.listen(3000);
 }
 bootstrap();
