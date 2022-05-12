98890cc7c5640f09d87893cb561fac9e8b27bdb7
xujx <xjxtju@163.com>
2022-05-12 23:39:11 +0800

    feat: add mobile repo

---
+++ apps/home-mobile-web/pages/index.tsx
@@ -0,0 +1,10 @@
+export function Index() {
+  return (
+    <div className="p-6">
+      <h1 className="text-red-400">this is home mobile</h1>
+      <span className="text-blue-300">based on tailwind.css</span>
+    </div>
+  );
+}
+
+export default Index;
